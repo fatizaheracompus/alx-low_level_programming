26dc7aca3ed796694e87e5809db2d2a49acc82ad
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2023-03-16 19:53:37 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-puts ("/"programming is like buildinga multilingual puzzle ") 
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
+

ad6abd3a9e4dde1d0bea4eaf7d01f1d53476cb5f
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2023-03-17 14:20:29 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,12 +19,8 @@ int main(void)
 		printf("%d is positive", n);
 	else if (n == 0)
 		printf("%d is zero", n);
-	else  
+	else
 		printf("%d is negative", n);
+	printf("\n");
 	return (0);
 }
-
-
-
-	
-	

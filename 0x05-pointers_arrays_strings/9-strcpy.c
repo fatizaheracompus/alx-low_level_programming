07f215b993d5409a0b25c4e2369abd0048baa5da
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2023-03-28 09:30:53 -0700

    task9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,5 +4,17 @@
  * _strcpy - copies the string pointed to by src
  * @dest: char type string
  * @src: char type string
- * Description : copy the string pointer 'src'''
+ * Description : copy the string pointer 'src'
+ * Return: copied string
+ */
 
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	if (dest == NULL)
+		return (NULL);
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	return (dest);
+}

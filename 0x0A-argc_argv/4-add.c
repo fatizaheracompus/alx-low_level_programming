62248c6f512af43f72747438c476c680a11152d5
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2023-04-30 18:53:12 -0700

    task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,57 +1,27 @@
-#include "main.h"
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * check_num - check - string there are digit
- * @str: array str
+ * main - function that add positive numbers
+ * @argc: The number of argument
+ * @argv: Array of pointer to arguments
  *
- * Return: Always 0 (success)
- */
-int check_num(char  *str)
-{
-	unsigned int count;
-
-	count = 0;
-	while (count < strlen(str))
-	{
-		if (!isdigit(str[count]))
-		{
-			return (0);
-		}
-		count++;
-	}
-	return (1);
-}
-
-/**
- * main - add  positive numbers.
- * @argc: numbers of arguments
- * @argv: array of arguments
- *
- * Return: always 0 (success)
+ * Return: Always 0.
  */
 
 int main(int argc, char *argv[])
 {
-	int count;
-	int str_to_int;
 	int sum = 0;
+	char *m;
 
-	count = 1;
-	while (count < argc)
+	while (--argc)
 	{
-		if (check_num(argv[count]))
-		{
-			str_to_int = atoi(argv[count]);
-			sum += str_to_int;
-		}
-		count++;
+		for (m = argv[argc]; *m; m++)
+			if (*m < '0' || *m > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);
-
 	return (0);
 }
 

a2e422615f86edd0eeef9c1afd135d41427a4504
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2023-04-30 22:24:48 -0700

    task0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,7 @@
 #include <limits.h>
 
 int _putchar(char c);
+void _puts(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);

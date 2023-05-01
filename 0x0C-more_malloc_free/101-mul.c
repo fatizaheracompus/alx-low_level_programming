6cde7c9f270b70322234d9f0775d24eed186c34e
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2023-05-01 11:40:35 -0700

    task5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,34 +1,92 @@
 #include "main.h"
 
 /**
- * main -  multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
+ * _puts - print string.
+ * @str: pointer to the string
+ *
+ * Return: void
  */
 
-int main(int argc, char *argv[])
+void _puts(char *str)
 {
-	unsigned long mul;
-	int i, j;
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _atoi - Convert a string to integer
+ * @s: Char type string
+ *
+ * Return: int
+ */
+int _atoi(const char *s)
+{
+	int sign = 1;
+	unsigned long int resp = 0, fNum, i;
+
+	for (fNum = 0; !(s[fNum] >= 48 && s[fNum] <= 57); fNum++)
+	{
+		if (s[fNum] == '-')
+		{
+			sign *= -1;
+		}
+	}
+	for (i = fNum; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
+
+	return (sign * resp);
+}
+
+/**
+ * print_int - print integer
+ * @m: integer
+ * Return: 0
+ */
+
+void print_int(unsigned long int m)
+{
+	unsigned long int divisor = 1, i, resp;
+
+	for (i = 0; m / divisor > 9; i++, divisor *= 10)
+		;
+	for (; divisor >= 1; m %= divisor, divisor /= 10)
+	{
+		resp = m / divisor;
+		_putchar('0' + resp);
+	}
+}
+
+/**
+ * main -  multiplies two positive numbers
+ * @argc: integer
+ * @argv: number of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
 
 	if (argc != 3)
 	{
-		printf("erro\n");
+		_puts("Error ");
 		exit(98);
 	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
-		}
-	}
-	mul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", mul);
-	return (0);
+
+		print_int(_atoi(argv[1]) * _atoi(argv[2]));
+		_putchar('\n');
+
+		return (0);
 }
+
+
+
+

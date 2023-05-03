6748ce487f4b9fa9165f623e17964362f7fce3ca
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2023-05-02 18:17:53 -0700

    task14

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * find_listint_loop - find a loop in linked list.
+ * @head: linked list
+ *
+ * Return: adress of node where loops start/NULL if no loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+listint_t *pt, *end;
+
+if (head == NULL)
+return (NULL);
+for (end = head->next; end != NULL; end = end->next)
+{
+if (end == end->next)
+return (end);
+for (pt = head; pt != end; pt = pt->next)
+if (pt == end->next)
+return (end->next);
+}
+return (NULL);
+}

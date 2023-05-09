ba6895396c03ae2e5ad7d292f5da757e0e88e22f
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2023-05-08 17:09:21 -0700

    task1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_file - function that creates a file.
+ * @filename: is the name of the file to create.
+ * @text_content:  is a NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp, w, L = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (L = 0; text_content[L]; L++)
+			;
+	}
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fp, text_content, L);
+
+	if (fp == -1 || w == -1)
+		return (-1);
+
+
+	close(fp);
+
+	return (1);
+}

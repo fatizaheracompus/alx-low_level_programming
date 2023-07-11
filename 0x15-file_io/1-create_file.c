f82139e3a7c687f2397260076c05c19706790f27
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2023-07-10 18:37:16 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,13 +19,13 @@ return (n);
 }
 
 /**
-*create_file - function create a file
-*@filename: name to file to be created.
-*@text_content: is a NULL terminated string to write to the file
+* create_file - function create a file
+* @filename: name to file to be created.
+* @text_content: is a NULL terminated string to write to the file
 *
-*Return: 1 on success, -1 on failure (file can not be created
-*file can not be written, write “fails”, etc).
+* Return: 1 on success, -1 
 */
+
 int create_file(const char *filename, char *text_content)
 {
 ssize_t byte = 0;
@@ -37,8 +37,6 @@ return (-1);
 fc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fc == -1)
 	return (-1);
-else
-	return (1);
 if (len)
 	byte = write(fc, text_content, len);
 close(fc);

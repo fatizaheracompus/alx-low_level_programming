19011cb3fc623cee844bfe3b96d3d9119cb3bc17
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2023-12-20 14:37:36 -0800

    rr

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -23,6 +23,6 @@ def island_perimeter(grid):
                 size += 1
                 if (b > 0 and grid[a][b - 1] == 1):
                     edges += 1
-                if (a > 0 and grid[b - 1][b] == 1):
+                if (a > 0 and grid[a - 1][b] == 1):
                     edges += 1
     return size * 4 - edges * 2
